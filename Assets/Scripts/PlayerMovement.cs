fe3c14d6d8e385d2eecee022fc32a352d295ca46
BGERGO7 <bukki.gergo@gmail.com>
2023-11-13 18:07:57 +0100

    Dead anim javitva

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,28 +15,30 @@ public class PlayerMovement : MonoBehaviour
     private float speed = 8f;
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
+    bool isDead = false;
 
     public int maxHealth = 100;
 	public int currentHealth;
+
 
     void Start()
     {
 		currentHealth = maxHealth;
 		healthBar.SetMaxHealth(maxHealth);
     }
-
+    
     void Update()
     {
-        if (!isFacingRight && horizontal > 0f)
+        if (!isDead && !isFacingRight && horizontal > 0f)
         {
             Flip();
         }
-        else if (isFacingRight && horizontal < 0f)
+        else if (!isDead && isFacingRight && horizontal < 0f)
         {
             Flip();
         }
 
-        if (!IsGrounded())
+        if (!isDead && !IsGrounded())
         {
             animator.SetBool("isJumping", true);
         } else
@@ -44,24 +46,21 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("isJumping", false);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.H))
 		{
 			TakeDamage(10);
 		}
-        else if (Input.GetKeyDown(KeyCode.H))
+
+        if (Input.GetKeyDown(KeyCode.Space))
 		{
 			AddHealth(10);
 		}
 
-        if(maxHealth <= 0)
+        if(currentHealth <= 0)
         {
-            Die();
+            animator.SetBool("isDead", true);
+            isDead = true;
         }
-    }
-
-    void Die()
-    {
-        animator.SetTrigger("Dead");
     }
 
     private void FixedUpdate()
@@ -72,12 +71,12 @@ public class PlayerMovement : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed && IsGrounded())
+        if (!isDead && context.performed && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
 
-        if (context.canceled && rb.velocity.y > 0f)
+        if (!isDead && context.canceled && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
@@ -86,19 +85,26 @@ public class PlayerMovement : MonoBehaviour
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        
     }
 
     private void Flip()
     {
-        isFacingRight = !isFacingRight;
-        Vector3 localScale = transform.localScale;
-        localScale.x *= -1f;
-        transform.localScale = localScale;
+        if (!isDead)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        horizontal = context.ReadValue<Vector2>().x;
+        if (!isDead)
+        {
+            horizontal = context.ReadValue<Vector2>().x;
+        }
     }
 
     void TakeDamage(int damage)
