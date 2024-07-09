b8db40c5b638aa57485acbcda6553a4494f660b9
BGERGO7 <bukki.gergo@gmail.com>
2024-07-09 19:45:37 +0200

    #38 Cooldown attack fixalva

--- Assets/AttackCooldown.cs
+++ Assets/AttackCooldown.cs
@@ -5,39 +5,32 @@ using UnityEngine.UI;
 
 public class AttackCooldown : MonoBehaviour
 {
-
     public Animator attack_anim;
     public Button attack_btn;
-    public MeleeAttack meleeAttack;
+    //public MeleeAttack meleeAttack;
 
     // Start is called before the first frame update
     void Start(){
-        attack_anim.SetBool("AttackCooldown", false);
         attack_btn.interactable = true;
-        meleeAttack = new MeleeAttack();
+        //meleeAttack = new MeleeAttack();
+        //attack_anim.SetTrigger("AttackCooldown");
     }
     
     void Awake()
     {
         attack_btn.onClick.AddListener(Button_pressed);
-        
     }
-
-
 
     void Button_pressed(){
-        attack_anim.SetBool("AttackCooldown", true);
         StartCoroutine(AttackCooldownStart());
-    
     }
+
     IEnumerator AttackCooldownStart(){
-      attack_anim.SetBool("AttackCooldown", true);
-      //meleeAttack.canAttack = false;
+      attack_anim.SetTrigger("AttackCooldown");
       attack_btn.interactable = false;
-      yield return new WaitForSeconds(1);
+      yield return new WaitForSecondsRealtime(1);
       Debug.Log("1 sec");
-      //meleeAttack.canAttack = true;
       attack_btn.interactable = true;
-      attack_anim.SetBool("AttackCooldown", false);
+      //attack_anim.SetBool("AttackCooldown", false);
     }
 }
