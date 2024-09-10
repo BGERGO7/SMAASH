6dcfea5bbe141b0494500cc33d27ff0263845c74
BDonat05 <146833446+BDonat05@users.noreply.github.com>
2024-09-10 09:26:41 +0200

    readme added (#40)

---
+++ Assets/Scripts/Environment_behaviours/parallax_effect.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class parallax_effect : MonoBehaviour
+{
+    private float Starting_pos;
+    public float Parallax_factor;
+    public Camera Camera;
+    private Vector3 Pos;
+    private float Distance;
+
+    private Camera FindCam()
+    {
+        GameObject[] Players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject p in Players)
+        {
+            PhotonView view = p.GetComponent<PhotonView>();
+            if (view != null && view.IsMine)
+            {
+            //   while (p.GetComponent<Camera>() == null)
+            //    {
+            //        Debug.Log("Fetching player cameras");
+            //    }
+                return p.GetComponent<Camera>();
+            }
+        }
+        return null;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Starting_pos = transform.position.x;
+        Camera = FindCam();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Pos = Camera.transform.position;
+        Distance = Pos.x * Parallax_factor;
+
+        transform.position = new Vector3(Starting_pos + Distance, transform.position.y, transform.position.z);
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+**Epic smash game**
