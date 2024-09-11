66e92af809691254a67d33f04380e152e48e438f
BGERGO7 <bukki.gergo@gmail.com>
2024-09-11 23:14:01 +0200

    Multiplayer bug fixálva #39

--- Assets/AttackCooldown.cs
+++ Assets/AttackCooldown.cs
@@ -1,3 +1,4 @@
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -39,3 +40,41 @@ public class AttackCooldown : MonoBehaviour
       attack_btn.interactable = true;
     }
 }
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+ public class AttackCooldown : MonoBehaviour
+ {
+     public Animator attack_anim;
+     public Button attack_btn;
+     //public MeleeAttack meleeAttack;
+
+     // Start is called before the first frame update
+     void Start(){
+         attack_btn.interactable = true;
+         //meleeAttack = new MeleeAttack();
+         //attack_anim.SetTrigger("AttackCooldown");
+     }
+
+     void Awake()
+     {
+         attack_btn.onClick.AddListener(Button_pressed);
+     }
+
+     void Button_pressed(){
+         StartCoroutine(AttackCooldownStart());
+     }
+
+     IEnumerator AttackCooldownStart(){
+       attack_anim.SetTrigger("AttackCooldown");
+       attack_btn.interactable = false;
+       yield return new WaitForSecondsRealtime(1);
+       Debug.Log("1 sec");
+       attack_btn.interactable = true;
+       //attack_anim.SetBool("AttackCooldown", false);
+     }
+ }
